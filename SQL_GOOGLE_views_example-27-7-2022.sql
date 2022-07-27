380e5125a4de52d836d10063c304c349f389ba1d
Arthur-spec-del <74678147+Arthur-spec-del@users.noreply.github.com>
2022-07-27 15:06:07 +0100

    Update SQL_GOOGLE_views_example-27-7-2022.sql

--- SQL_GOOGLE_views_example-27-7-2022.sql
+++ SQL_GOOGLE_views_example-27-7-2022.sql
@@ -1,20 +1,14 @@
-CREATE TABLE IF NOT EXISTS SQL_GOOGLE_views_example_27_7_2022 (
-    `Number_of_mentions_of_Google_on_Wikipedia` VARCHAR(48) CHARACTER SET utf8,
-    `Column_2` INT
-);
-INSERT INTO SQL_GOOGLE_views_example_27_7_2022 VALUES
-    (NULL,NULL),
-    ('SELECT',NULL),
-    ('  language',NULL),
-    ('  title',NULL),
-    ('  SUM(views) AS views',NULL),
-    ('FROM',NULL),
-    ('  `bigquery-samples.wikipedia_benchmark.Wiki10B`',NULL),
-    ('WHERE',NULL),
-    ('  title LIKE ''%Google%''',NULL),
-    ('GROUP BY',NULL),
-    ('  language',NULL),
-    ('  title',NULL),
-    ('ORDER BY',NULL),
-    ('  views DESC;',NULL),
-    (NULL,NULL);
+--SQL_GOOGLE_views_example_27_7_2022
+SELECT
+    language
+    title
+    SUM(views) AS views
+FROM
+    `bigquery-samples.wikipedia_benchmark.Wiki10B`
+WHERE
+    title LIKE ''%Google%'
+GROUP BY
+    language,
+    title
+ORDER BY
+    views DESC;

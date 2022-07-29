f24361ea69bcc7f5dc49b78218a0f50cda54519f
Arthur-spec-del <74678147+Arthur-spec-del@users.noreply.github.com>
2022-07-29 09:57:37 +0100

    Update SQL_INNER_JOIN_avg_spend_world_bank-28-7-2022.sql

--- SQL_INNER_JOIN_avg_spend_world_bank-28-7-2022.sql
+++ SQL_INNER_JOIN_avg_spend_world_bank-28-7-2022.sql
@@ -1,15 +1,13 @@
-CREATE TABLE IF NOT EXISTS SQL_INNER_JOIN_avg_spend_world_bank_28_7_2022 (
-    `SELECT` VARCHAR(83) CHARACTER SET utf8,
-    `Column_2` VARCHAR(15) CHARACTER SET utf8
-);
-INSERT INTO SQL_INNER_JOIN_avg_spend_world_bank_28_7_2022 VALUES
-    ('    AVG(edu.value) average_value',' summary.region'),
-    ('FROM ',NULL),
-    ('    `bigquery-public-data.world_bank_intl_education.international_education` AS edu',NULL),
-    ('INNER JOIN ',NULL),
-    ('    `bigquery-public-data.world_bank_intl_education.country_summary` AS summary',NULL),
-    ('ON edu.country_code = summary.country_code',NULL),
-    ('WHERE summary.region IS NOT null',NULL),
-    ('GROUP BY summary.region',NULL),
-    ('ORDER BY average_value DESC',NULL),
-    (NULL,NULL);
+
+SELECT
+  AVG(edu.value) AS average_value,
+  summary.region
+FROM
+`bigquery-public-data.world_bank_intl_education.international_education` AS edu
+INNER JOIN
+`bigquery-public-data.world_bank_intl_education.country_summary` AS summary
+    ON edu.country_code = summary.country_code
+    WHERE summary.region
+    GROUP BY summary.region
+    ORDER BY average_value DESC
+    

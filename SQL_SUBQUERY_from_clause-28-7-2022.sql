ca8d38fda696819a3a85d286d8d9d934ed2ba471
Arthur-spec-del <74678147+Arthur-spec-del@users.noreply.github.com>
2022-07-29 11:27:58 +0100

    Update SQL_SUBQUERY_from_clause-28-7-2022.sql

--- SQL_SUBQUERY_from_clause-28-7-2022.sql
+++ SQL_SUBQUERY_from_clause-28-7-2022.sql
@@ -1,23 +1,19 @@
-CREATE TABLE SQL_SUBQUERY_from_clause_28_7_2022 (
-    `SELECT` VARCHAR(93) CHARACTER SET utf8,
-    `Column_2` INT
-);
-INSERT INTO SQL_SUBQUERY_from_clause_28_7_2022 VALUES
-    ('  station_id',NULL),
-    ('  name',NULL),
-    ('  num_of_rides AS number_of_rides_starting_at_station',NULL),
-    ('FROM',NULL),
-    ('  (',NULL),
-    ('    SELECT',NULL),
-    ('    start_station_id',NULL),
-    ('    COUNT(*) num_of_rides',NULL),
-    ('    FROM ',NULL),
-    ('      `bigquery-public-data.new_york_citibike.citibike_trips`',NULL),
-    ('      GROUP BY',NULL),
-    ('        start_station_id',NULL),
-    ('  ) AS station_num_trips',NULL),
-    ('  INNER JOIN',NULL),
-    ('  `bigquery-public-data.new_york_citibike.citibike_stations` ON station_id = start_station_id',NULL),
-    ('  ORDER BY',NULL),
-    ('    num_of_rides DESC',NULL),
-    (NULL,NULL);
+SELECT
+ station_id
+ name
+ num_of_rides AS number_of_rides_starting_at_station
+FROM
+    (SELECT
+        start_station_id
+        COUNT(*) num_of_rides
+        FROM
+        `bigquery-public-data.new_york_citibike.citibike_trips`
+        GROUP BY
+        start_station_id
+        ) AS station_num_trips
+INNER JOIN
+    `bigquery-public-data.new_york_citibike.citibike_stations`
+    ON station_id = start_station_id'
+ORDER BY
+    num_of_rides DESC
+    

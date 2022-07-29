429f4aa711f2ffc8758b3a39ee8ef7ffae14dca5
Arthur-spec-del <74678147+Arthur-spec-del@users.noreply.github.com>
2022-07-29 11:33:21 +0100

    Update SQL_SUBQUERY_where_clause-28-7-2022.sql

--- SQL_SUBQUERY_where_clause-28-7-2022.sql
+++ SQL_SUBQUERY_where_clause-28-7-2022.sql
@@ -1,20 +1,16 @@
-CREATE TABLE SQL_SUBQUERY_where_clause_28_7_2022 (
-    `SELECT` VARCHAR(61) CHARACTER SET utf8,
-    `Column_2` INT
-);
-INSERT INTO SQL_SUBQUERY_where_clause_28_7_2022 VALUES
-    ('  station_id',NULL),
-    ('  name',NULL),
-    ('FROM',NULL),
-    ('  `bigquery-public-data.new_york_citibike.citibike_stations`',NULL),
-    ('WHERE',NULL),
-    ('  station_id IN',NULL),
-    ('    (',NULL),
-    ('    SELECT',NULL),
-    ('      start_station_id',NULL),
-    ('    FROM',NULL),
-    ('      `bigquery-public-data.new_york_citibike.citibike_trips`',NULL),
-    ('    WHERE',NULL),
-    ('      usertype = "Subscriber"',NULL),
-    ('    )',NULL),
-    (NULL,NULL);
+
+SELECT
+    station_id
+    name
+    FROM
+     `bigquery-public-data.new_york_citibike.citibike_stations`
+    WHERE
+     station_id IN
+     (
+     SELECT
+      start_station_id
+      FROM
+       `bigquery-public-data.new_york_citibike.citibike_trips`
+      WHERE
+        usertype = "Subscriber"
+      )

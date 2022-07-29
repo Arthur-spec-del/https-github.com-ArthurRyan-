57f2980c8d661c3cfcc6e23711485ec2f5a6f7b1
Arthur-spec-del <74678147+Arthur-spec-del@users.noreply.github.com>
2022-07-29 11:08:43 +0100

    Update SQL_OUTER_JOIN-28-7-2022.sql

--- SQL_OUTER_JOIN-28-7-2022.sql
+++ SQL_OUTER_JOIN-28-7-2022.sql
@@ -1,13 +1,10 @@
-CREATE TABLE IF NOT EXISTS SQL_OUTER_JOIN_28_7_2022 (
-    `SELECT` VARCHAR(68) CHARACTER SET utf8,
-    `Column_2` INT
-);
-INSERT INTO SQL_OUTER_JOIN_28_7_2022 VALUES
-    ('  employee_data.name AS department_name',NULL),
-    ('  employee_data.department_id AS department_id',NULL),
-    ('  departments.name AS employee_name',NULL),
-    ('FROM `st-project-18-7-2022.SQL_demos.employee_data` AS employee_data',NULL),
-    ('FULL OUTER JOIN',NULL),
-    ('  `st-project-18-7-2022.SQL_demos.departments` AS departments ON',NULL),
-    ('  employee_data.department_id = departments.department_id',NULL),
-    (NULL,NULL);
+SELECT
+ employee_data.name AS department_name
+ employee_data.department_id AS department_id
+ departments.name AS employee_name
+FROM `st-project-18-7-2022.SQL_demos.employee_data` AS employee_data
+FULL OUTER JOIN
+ `st-project-18-7-2022.SQL_demos.departments` AS departments
+ON
+ employee_data.department_id = departments.department_id
+    
